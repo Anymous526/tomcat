a86d712b91033beef3471e144371ecefb72fbdd7
Anymous526 <kevin.sun526@qq.com>
2016-06-14 16:01:49 +0800

    formatter

---
+++ src/live/wallet/tomcat/v4/startup/Bootstrap.java
@@ -0,0 +1,23 @@
+package live.wallet.tomcat.v4.startup;
+
+import org.apache.catalina.connector.http.HttpConnector;
+
+import ex04.pyrmont.core.SimpleContainer;
+
+public class Bootstrap {
+	public static void main(String[] args) {
+		HttpConnector connector = new HttpConnector();
+		SimpleContainer container = new SimpleContainer();
+		connector.setContainer(container);
+		try {
+			connector.initialize();
+			connector.start();
+
+			// make the application wait until we press any key.
+			System.in.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
