165e44f2a3a33af0a6f20e75c5307c475b5a22f6
Anymous526 <kevin.sun526@qq.com>
2016-06-20 12:00:30 +0800

    finished V15

--- AnalyseTomcat/src/ex15/pyrmont/digestertest/Test01.java
+++ AnalyseTomcat/src/ex15/pyrmont/digestertest/Test01.java
@@ -6,7 +6,7 @@ import org.apache.commons.digester.Digester;
 public class Test01 {
 
   public static void main(String[] args) {
-    String path = System.getProperty("user.dir") + File.separator  + "etc";
+    String path = System.getProperty("user.dir") + File.separator  + "AnalyseTomcat/etc";
     File file = new File(path, "employee1.xml");
     Digester digester = new Digester();
     // add rules
